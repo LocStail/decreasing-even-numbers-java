bfbf0d3128a88dc3f9f7abca8cd71dda149f1d66
Erekle <kiknadze.erekle24@gtu.ge>
2026-06-13 19:03:09 +0400

    Rewriten in while loop

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i = 100; i >= 1; i--){
-            if(i % 2 == 0){
+        int i = 100;
+        while(i >= 1) {
+            if(i % 2 == 0) {
                 System.out.print(i + " ");
             }
+            i--;
         }
+
     }
 }
